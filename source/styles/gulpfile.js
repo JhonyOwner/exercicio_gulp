a9659e1eb52fa617e44ff1f1419ddbfb2b7d86d9
JhonyOwner <jvgbrisola@gmail.com>
2025-02-19 11:23:40 -0300

    Instalado o --save-dev gulp-sass e o sass

---
+++ source/styles/gulpfile.js
@@ -0,0 +1,8 @@
+const sass = require("gulp-sass")(require("sass"));
+
+function compilaSass() {
+  return gulp
+    .src("./source/styles/.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./build/styles"));
+}
